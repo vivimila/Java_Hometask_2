b95fca533103ba98959c53b2f62f9b10ec94764e
vivimila <violamilashevskaya@yandex.by>
2023-01-19 16:59:50 +0300

    Solved the task №2

--- ProgramMirror.java
+++ ProgramMirror.java
@@ -2,5 +2,24 @@
 // являются ли две данные строки вращением друг друга (вхождение в обратном порядке).
 
 public class ProgramMirror {
-    
+    class StringRotation
+{
+    static boolean areRotations(String str1, String str2)
+    {
+        return (str1.length() == str2.length()) &&
+               ((str1 + str1).indexOf(str2) != -1);
+    }
+
+    public static void main (String[] args)
+    {
+        String str1 = "AACCC";
+        String str2 = "CCAAA";
+  
+        if (areRotations(str1, str2))
+            System.out.println("Строки зеркальные друг другу");
+        else
+            System.out.printf("Строки не зеркальные друг другу");
+    }
+}
+  
 }

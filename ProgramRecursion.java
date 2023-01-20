887023cc9a26cbcea85bfc28759448c099c935f1
vivimila <violamilashevskaya@yandex.by>
2023-01-20 14:46:46 +0300

    Solved the task №3

--- ProgramRecursion.java
+++ ProgramRecursion.java
@@ -1,10 +1,21 @@
 // 3. *Напишите программу, чтобы перевернуть строку с помощью рекурсии.
 
 public class ProgramRecursion {
-    public static String reverseStr(String str) {
-        if (str.length() <= 1) {
-            return str;
-        }
-        return reverseStr(str.substring(1)) + str.charAt(0);
+    public static void main(String[] args) {
+    String s = "Java\n";
+    System.out.print(s);
+        
+    if(s.length() > 0) {
+        reverseString(s, s.length() - 1);
+    }
+    }
+    public static void reverseString(String s, int index) {
+    if(index == 0) {
+        System.out.println(s.charAt(index));
+            
+         return;
+    }
+    System.out.print(s.charAt(index));
+    reverseString(s, index - 1);
     }
 }

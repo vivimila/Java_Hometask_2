57fddea17813df1ff5f123bad2ef7f34b8746dbb
vivimila <violamilashevskaya@yandex.by>
2023-01-20 14:34:02 +0300

    Changed the task 3

--- ProgramRecursion.java
+++ ProgramRecursion.java
@@ -1,5 +1,10 @@
 // 3. *Напишите программу, чтобы перевернуть строку с помощью рекурсии.
 
 public class ProgramRecursion {
-    
+    public static String reverseStr(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        return reverseStr(str.substring(1)) + str.charAt(0);
+    }
 }
